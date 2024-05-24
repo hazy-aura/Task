666c2048cfa7b276fc5d66fbd3d97beb6824173a
Ayush Rana <108958206+hazy-aura@users.noreply.github.com>
2024-05-24 17:42:12 +0530

    finishing  touch

--- components/MiddleNavLinks.jsx
+++ components/MiddleNavLinks.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 function MiddleNavLinks() {
  return(
-    <div className=" sticky z-1 flex items-start justify-center space-x-12 w-115 pt-12  ">
+    <div className=" sticky z-1 flex items-start justify-center space-x-12 w-115 pt-12 text-lg  ">
     <Link href={"/"}> Bags</Link>
     <Link href={"/"}> Travel</Link>
     <Link href={"/"}> Accessories</Link>
